15354651ab7e448f19305998df8830459e56ecb6
BailsBails1 <147569909+BailsBails1@users.noreply.github.com>
2025-03-19 23:41:16 +0000

    started to do list

--- to-do-list.py
+++ to-do-list.py
@@ -6,3 +6,45 @@
 - Class: Software Engineering
 - Description: Complete a functional dice roller app in Python.
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+#title
+print("welcome to your to do list!") 
+
+def menu():
+
+    print("1 to add to the list")
+    print("2 to remove from list")
+    print("3 to view list")
+    print("4 to exit")
+    ans=input("what is your choice")
+
+
+    if ans== "1":
+        add_list()
+
+def add_list():
+
+    while True:
+        new=input("please enter something for your list")
+        list.append(new)
+
+        contin = input("continue?: ")
+        if contin == "a":
+            return
+
+
+
+
+
+
+
+
+def my_list():
+    list=[]
+    list.sort()
+
+
+
+
+
